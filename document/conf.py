a0c383844733a4c06290ae22a8ae7dd5a9857473
HUANG SIZHE <hsz1273327@gmail.com>
2018-03-10 01:08:01 +0800

    doc-update:Sat Mar 10 01:08:01 2018

--- document/conf.py
+++ document/conf.py
@@ -26,7 +26,7 @@ project = 'pmfp'
 copyright = '2017, hsz'
 author = 'hsz'
 
-version = '1.1.1'
+version = '2.0.0'
 
 release = ''
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 REQUIREMETS_DEV_FILE = 'requirements_dev.txt'
 REQUIREMETS_FILE = 'requirements.txt'
 PROJECTNAME = 'pmfp'
-VERSION = '1.1.1'
+VERSION = '2.0.0'
 DESCRIPTION = 'a simple package manager for python like npm.'
 URL = 'https://github.com/Python-Tools/pmfp'
 AUTHOR = 'hsz'
