4e6f831220019469f05bd10c4355d0e1c1cfab82
HUANG SIZHE <hsz1273327@gmail.com>
2021-05-07 23:43:15 +0800

    update

--- document/conf.py
+++ document/conf.py
@@ -22,10 +22,10 @@ copyright = '2021, hsz'
 author = 'hsz'
 
 # The short X.Y version
-version = '4.0.3'
+version = '4.0.4'
 
 # The full version, including alpha/beta/rc tags
-release= '4.0.3'
+release= '4.0.4'
 
 
 # -- General configuration ---------------------------------------------------
